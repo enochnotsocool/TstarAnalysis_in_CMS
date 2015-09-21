367f32f12093713fd26f4cd35732175c6cfbdd27
Enoch Chen <enochnotsocool@gmail.com>
2015-09-21 10:59:16 +0200

    First remote commit

---
+++ EventAnalyzer/interface/EventAnalyzer.h
@@ -0,0 +1,25 @@
+#include <memory>
+
+#include "FWCore/Framework/interface/Frameworkfwd.h"
+#include "FWCore/Framework/interface/one/EDAnalyzer.h"
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+
+class EventAnalyzer : public edm::one::EDAnalyzer<edm::one::SharedResources>  {
+   public:
+      explicit EventAnalyzer(const edm::ParameterSet&);
+      ~EventAnalyzer();
+
+      static void fillDescriptions(edm::ConfigurationDescriptions& descriptions);
+
+
+   private:
+      virtual void beginJob() override;
+      virtual void analyze(const edm::Event&, const edm::EventSetup&) override;
+      virtual void endJob() override;
+
+      // ----------member data ---------------------------
+};
+
+
